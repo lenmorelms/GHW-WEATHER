966ac4673927b454b6500354ce2b88142c0571dc
lenmorelms <learnmorelms@gmail.com>
2024-04-11 09:10:34 +0200

    Added .env file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import fetch from 'node-fetch';
-const API_KEY = '5f6913c1c715dd5b4230b8b432630bb6';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const latitude = '-17.858560';
 const longitude = '31.024742';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`;
 
 fetch(apiUrl)
   .then(response => {
